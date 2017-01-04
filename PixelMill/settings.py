e6751341bdf9913b95fba90eb86c274749cea93e
GoGo <tygogo@outlook.com>
2017-01-05 00:57:12 +0800

    开发配置生产配置分离。=。=

--- PixelMill/settings.py
+++ PixelMill/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'zh_hans'
 
 TIME_ZONE = 'UTC'
 
